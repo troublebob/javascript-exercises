e288f056ac10036543fbf74d61309302ce7e6c26
Robert Foran <Robert.Foran@ucdconnect.ie>
2018-08-10 14:57:19 +0100

    sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    if(typeof(a)!="number"||typeof(b)!="number"){
+        return "ERROR"
+    }
+    if(a<0||b<0){
+        return "ERROR"
+    }
+    let i = [a,b]
+    i = i.sort(function(a,b){return a - b})
+    let result = 0
+    for(i[0];i[0]<=i[1];i[0]++){
+        result+=i[0]
+    }
+    return result
 }
 
 module.exports = sumAll
