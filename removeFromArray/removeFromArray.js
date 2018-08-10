44b3420885d506dbf49bf70d0eb5e084549ad5bb
Robert Foran <Robert.Foran@ucdconnect.ie>
2018-08-10 12:12:31 +0100

    repeat string added

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -3,12 +3,3 @@ const removeFromArray = function(array, ...targets) {
 }
 
 module.exports = removeFromArray
-
-
-/* const arrayToBeChecked =array;
-    const elementsToBeRemoved = targets;
-    arrayToBeChecked.forEach((element1)=>elementsToBeRemoved.forEach((element2)=>
-        {if (element1!=element2){
-            arrayToBeChecked.remove(element1)
-        }}))
-    return arrayToBeChecked */
--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, count) {
+    if(count < 0){return "ERROR"}
+    let s = ""
+    for(count;count>0;count--){
+        s+=string
+    }
+    return s
 }
 
 module.exports = repeatString
