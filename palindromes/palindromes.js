7ea8ee550609d0b7ae64a1d7a54a566ddc584f0f
Robert Foran <Robert.Foran@ucdconnect.ie>
2018-08-10 14:03:39 +0100

    Palindromes added

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function() {
-
+const palindromes = function(string) {
+    let punctuation =['?','.','!',' ',',']
+    let tester = string.toLocaleLowerCase().split("")
+    tester=tester.filter((element) => !punctuation.includes( element ));
+    tester=tester.join("")
+    if(tester===tester.split("").reverse().join("")){
+        return true
+    }else{
+        return false
+    }
+    
 }
-
 module.exports = palindromes
