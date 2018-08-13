ffbab55f2c2186b91738e632bdb2cce95284e379
Robert Foran <Robert.Foran@ucdconnect.ie>
2018-08-13 09:34:49 +0100

    Temp conversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(t) {
+  t-=32
+  return Math.round(t * 5/9 * 10)/10
 }
 
-const ctof = function() {
-
+const ctof = function(t) {
+  t*=1.8
+  return Math.round((t+=32 )* 10)/10
 }
 
 module.exports = {
