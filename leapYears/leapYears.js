fdebe1a9a3c0850ada228e0d553141f6090bbc4c
Robert Foran <Robert.Foran@ucdconnect.ie>
2018-08-13 09:19:06 +0100

    leapyear added

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(years) {
+    if(years % 400 == 0){
+        return true
+    }else if(years % 100 == 0){
+        return false
+    }else if(years % 4 == 0){
+        return true
+    }else{
+        return false
+    }
 }
 
 module.exports = leapYears
--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,15 +1,15 @@
-const sumAll = function(a,b) {
-    if(typeof(a)!="number"||typeof(b)!="number"){
+const sumAll = function (a, b) {
+    if (typeof (a) != "number" || typeof (b) != "number") {
         return "ERROR"
     }
-    if(a<0||b<0){
+    if (a < 0 || b < 0) {
         return "ERROR"
     }
-    let i = [a,b]
-    i = i.sort(function(a,b){return a - b})
+    let i = [a, b]
+    i = i.sort(function (a, b) { return a - b })
     let result = 0
-    for(i[0];i[0]<=i[1];i[0]++){
-        result+=i[0]
+    for (i[0]; i[0] <= i[1]; i[0]++) {
+        result += i[0]
     }
     return result
 }
